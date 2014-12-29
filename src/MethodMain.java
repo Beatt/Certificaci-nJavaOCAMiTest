e1db0a7d6e3181c755299f4df17520c226dc9eaa
Gabriel <beat_@comunidad.unam.mx>
2014-12-29 16:59:31 -0600

    29/12/14

---
+++ src/MethodMain.java
@@ -0,0 +1,43 @@
+
+public class MethodMain {
+	
+	/*
+	 * 
+	 * El método Main puede contoner los siguientes
+	 * 	modificadores de acceso como los de no-acceso
+	 * 
+	 * 		Acceso
+	 * 			public
+	 * 
+	 * 		No-Acceso
+	 * 			final
+	 * 			static
+	 * 
+	 * De los cuales public y static son obligatorios para que la 
+	 * 	JVM permita ejecutarlo...
+	 * 
+	 * 
+	 */
+	
+	
+	/*public static void main(String[] args) {
+		System.out.println("public static void main");
+	}
+	
+	public final static void main(String[] args) {
+		System.out.println("public final static void main");
+	}
+	
+	static final public void main(String[] args) {
+		System.out.println("static final public void");
+	}	
+	
+	final static public void main(String[] args) {
+		System.out.println("final static public void main");
+	}
+	 
+	final public static void main(String[] args) {
+		System.out.println("final public static void main");
+	}*/
+
+}
